53d111f2f00c035c35d7b06536af0fb8a18c3c1a
ahmednasr1237 <92389822+ahmednasr1237@users.noreply.github.com>
2023-08-25 06:21:32 +0300

    delete comments

--- [TEMPLATE]/EdgeTypes/EdgeTypes.cs
+++ [TEMPLATE]/EdgeTypes/EdgeTypes.cs
@@ -6,19 +6,7 @@ using System.Threading.Tasks;
 
 namespace Problem
 {
-  /*  class DFS_Shared_Variables
-    {
-        public Byte[] NodeColors;
-        public int CountEdges;
-        public int[] ParentNodes;
-
-        public DFS_Shared_Variables(int length)
-        {
-            NodeColors = new Byte[length];
-            ParentNodes = new int[length];
-        }
-    }
-    */
+  
     public static class EdgeTypes
     {
         #region YOUR CODE IS HERE
@@ -35,38 +23,14 @@ namespace Problem
         {
             List<int>[] GraphListNodes = new List<int>[vertices.Length];
 
-            //.. all nodes not visited by Make the boolean flag equals "false" 
-
-            /*
-                          #### Psoudo code ####
-                   for each vertex u in Vertcies
-                         color[u] = 0  => this indecate white color.
-
-           */
+            
             Byte[] _nodeColors_ = new Byte[vertices.Length]; // contains for each vertix 0 (white) OR 1 (Gray)
-            /*  foreach (int vertex in vertices)
-              {
-                  nodeColors[vertex] =0;
-              }*/
-
-            // bool[] visited_Node = new bool[vertices.Length];
-            /* for (int v = 0; v < vertices.Length; v++) // increase the time so the defualt of bool is false and i make all visited_Node false without loop.
-             {
-                 visited_Node[ v ]= false;
-             }*/
-
-
-            //.. make  Backword and Foroward and cross Edge counter intially values= zeros in each counter.
-            // int backwardEdgeCount = 0, forwardEdgeCount = 0, crossEdgeCount = 0;
-            // incremented when it arrive to Node not visited and Not parentNode.
-
+            
             int _countEdges_ = 0;
 
             int[] parentNodes = new int[vertices.Length];
-            //int[] Backword_Foroward_crossEdge_count = { 0, 0, 0 };
 
 
-            // **Create the Graph**
             foreach (int Vertix in vertices)// ..add the vertices in the list.
             {
                 GraphListNodes[Vertix] = new List<int>();
@@ -77,104 +41,39 @@ namespace Problem
                 GraphListNodes[Edge.Value].Add(Edge.Key);
             }
 
-           // DFS_Shared_Variables SharedVars = new DFS_Shared_Variables(vertices.Length);
-
-            /*
-             ## ANOTHER LOGIC ##
-
-              HashSet<int> visitedNodes = new HashSet<int>();
-            int edgeCount = 0;
-
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                if (!visitedNodes.Contains(i))
-                {
-                    DFSAlgorithm(i, GraphList, visitedNodes, ref edgeCount, -1);
-                }
-            }
-
-            return new int[] { edgeCount, edgeCount, 0 };
-        }
-             */
+          
 
 
-            // .. check if i visit this node or not yet.
-
-            /*
-                     #### Psoudo code ####
-
-                for each vertex Adj in Vertcies
-                    if color[Adj] ==0 then 
-                     		DFS-Visit(Adj)
-             */
-
+           
             foreach (int _vertex_ in vertices)
             {
                 if (_nodeColors_[_vertex_] == 0)
                 {
-                    /*
-                        SharedVars.ParentNodes[vertex] = vertex;
-                        DFSAlgorithm_Visit(vertex, GraphListNodes, SharedVars);
-                     */
 
                     parentNodes[_vertex_] = _vertex_;
                     DFSAlgorithm_Visit(_vertex_, GraphListNodes, parentNodes, ref _countEdges_, _nodeColors_);
                 }
             }
 
-
-            /* foreach (int i in vertices)
-             {
-                 if (!visited_Node[i]) // if not yet => apply DFS
-                 {
-                     DFSAlgorithm(i, adjacency_list, visited_Node, ref countEdges, -100);
-                 }
-             }*/
-            // .. finally return the count of each Edge
             return new int[] { _countEdges_, _countEdges_, 0 }; // BackwordCount = ForwordCount && CrossCount =0 becouse it Undirected graph.
         }
 
         private static void DFSAlgorithm_Visit(int vertix, List<int>[] GraphListNodes, int[] parentNode, ref int _countEdges_, Byte[] _nodeColors_)
         {
-            //   visitedNodes.Add(vertex);
-            // SharedVars.NodeColors[vertex] = 1;
+            
             _nodeColors_[vertix] = 1; // become this node visited.   **discovered**
 
-            //  loop adjacent vertex u of v "from LECTURE"
-
-            /*
-             
-                  #### Psoudo code ####
-
-                  for each v in Adj[Node]
-                       if color[v] = WHITE then 	**new**
-       	                   DFS-Visit(v)
-
-             */
-
+            
             foreach (int Adjacent in GraphListNodes[vertix])
             {
-                // if (!visitedNodes.Contains(adjacent))
                 if (_nodeColors_[Adjacent] == 0) // check it visited or not?
                 {
                     parentNode[Adjacent] = vertix;
-                    //if not .. applay DFS Algorithm on it again.
-                    // DFSAlgorithm_Visit(adjacent, GraphListNodes, SharedVars);
                     DFSAlgorithm_Visit(Adjacent, GraphListNodes, parentNode , ref _countEdges_, _nodeColors_);
                 }
-                // if it visited.. it is the parent? so ignore it
-                /*  else if (Adjacent == parentNode)
-                  {
-                      continue;
-                  }*/
-                // if it visited.. it is the parent? so check if this node backword , forword , cross or not all of them.
-                //else if (adjacent != SharedVars.ParentNodes[vertex] && vertex < adjacent)
                 else if (Adjacent != parentNode[vertix] && vertix < Adjacent)
                 {
-                    //  If u is visited and u is not v's parent, then classify the edge from v to u. && detect the backword age
                     _countEdges_++; //detect the backword age
-                                    // else if (vertix > Adjacent) { forwardEdgeCount++; }//detect  the forward edge
-                                    // else { crossEdgeCount++; } // detect cross edge
                 }
             }
         }
